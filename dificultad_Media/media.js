8d2eb34d6eefbbe6f79b551b9a9b7d8f19ecc8ee
Chasky91 <david_c91@outlook.com>
2024-11-14 09:49:40 -0300

    nuevos ejercicios

--- dificultad_Media/media.js
+++ dificultad_Media/media.js
@@ -17,23 +17,32 @@ function crearMatrizDiagonal(n) {
 
 
 // Cuenta las vocales en cada palabra de un array
+// recibe un array
 function contarVocalesPorPalabra(palabras) {
-    let resultados = {};
+    // se creara un objeto json, creando las propiedades de 
+    let resultados = {}
     for (let i = 0; i < palabras.length; i++) {
-        let conteoVocales = 0;
+        // defino la variable conteo de vocales en 0
+        let conteoVocales = 0
+        // creo por cada palabra en el array una nueva variable
         let palabra = palabras[i]
         console.log(palabra)
+        // recorro cada caracter de cada palabra, por la propiedad lenth
         for (let j = 0; j < palabra.length; j++) {
-            console.log(palabra[j])
+            // reviso con un if si algun caracter esta incluye o es aeiou
             if ('aeiou'.includes(palabra[j].toLowerCase())) {
                 conteoVocales++;
             }
         }
+        // esto parece un array pero estoy agrgando una clave a unobjeto json
+        // resultados = {}
+        // resultado.palabra --> resultado.roma
         resultados[palabra] = conteoVocales;
     }
     return resultados;
 }
-//console.log(contarVocalesPorPalabra(['roma', 'cruz', 'Esteban']));
+//paso un array sin crear una variable
+console.log(contarVocalesPorPalabra(['roma', 'cruz', 'Esteban']));
 
 
 
@@ -57,7 +66,7 @@ const vocales = ["a","e","i","o","u"]
 }*/
 
 const palabras = []
-for (let i = 0; i < 5; i++) {
+/*for (let i = 0; i < 5; i++) {
     let pregunta = "ingresa  los  dias de la semana : "
     let palabra = await obtenerEntrada(pregunta )
     console.log("\n")
@@ -70,7 +79,39 @@ async function validar() {
         console.log("\n")
         palabras.push(palabra)    
     }
+}*///
+
+
+//console.log(palabras);
+
+
+const temperaturas = [-5, 10, 25, 40, -2, 36, 15, 28, 39];
+const temperaturasExtremas = [];
+
+for (let i = 0; i < temperaturas.length; i++) {
+    // si la temperatura es menor a 0 grados o mayor a 35 se agrega 
+    // dentro del array
+    if (temperaturas[i] < 0 || temperaturas[i] > 35) {
+        temperaturasExtremas.push(temperaturas[i]);
+    }
 }
+console.log(temperaturasExtremas)
 
+const numeros = [5, 12, 8, 21, 14, 3, 18];
+const pares = [];
 
-console.log(palabras);
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0 && numeros[i] > 10) {
+        pares.push(numeros[i])
+    }
+}
+console.log(pares)
+
+function contarHaciaAtras(n) {
+    if (n === 0) {
+        return; // Caso base: si n llega a 0, termina la función
+    } else {
+        console.log(n); // Imprimir el número actual
+        contarHaciaAtras(n - 1); // Llamada recursiva con n reducido en 1
+    }
+}
